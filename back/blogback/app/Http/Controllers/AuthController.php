d64185abfba04c1605ff970f5205f908892d7045
malekbannour420 <malek.bannour@isimg.tn>
2026-05-10 11:30:47 +0100

    Add blank lines for improved readability in AuthController

--- back/blogback/app/Http/Controllers/AuthController.php
+++ back/blogback/app/Http/Controllers/AuthController.php
@@ -9,6 +9,8 @@ use Illuminate\Validation\ValidationException;
 use App\Http\Controllers\Controller;
 
 
+
+
 class AuthController extends Controller
 {
     public function register(Request $request)
